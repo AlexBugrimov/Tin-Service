a58b06f6280dee6e20cc7220abafdb1b2c4d9f35
Alex <bugrimov_a@bk.ru>
2021-11-24 22:11:11 +0300

    :art: Initialization project

---
+++ src/main/java/dev/bug/tinkoffservice/TinkoffServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.bug.tinkoffservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkoffServiceApplication.class, args);
+	}
+
+}
